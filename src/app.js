dec5b87b9e755151a025863e23af41e672b2d16f
VolodymyrKolisnichenko <90705859+vk-workshop@users.noreply.github.com>
2024-04-01 11:15:28 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,40 @@
-// write code here
+/* eslint-disable no-console */
+const fs = require('fs').promises;
+const path = require('path');
+
+async function moveFile() {
+  try {
+    const [pathFrom, pathTo] = process.argv.slice(2);
+
+    const absPathFrom = path.resolve(pathFrom);
+    const absPathTo = path.resolve(pathTo);
+
+    if (!fs.existsSync(absPathFrom)) {
+      throw new Error('Path does not exist!');
+    }
+
+    const sourceStats = await fs.stat(absPathFrom);
+
+    if (!sourceStats.isFile()) {
+      throw new Error('There is no file!');
+    }
+
+    let destinationPath = absPathTo;
+
+    if (absPathTo.endsWith('/')) {
+      const fileName = path.basename(pathFrom);
+
+      destinationPath = path.join(absPathTo, fileName);
+
+      if (!fs.existsSync(absPathTo)) {
+        throw new Error('Path already exists!');
+      }
+    }
+
+    await fs.rename(absPathFrom, destinationPath);
+  } catch (err) {
+    console.error('Error:', err.message);
+  }
+}
+
+moveFile();
